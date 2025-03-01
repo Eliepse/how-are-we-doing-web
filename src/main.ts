2086a448611a6b578660580b9947fe9334c3ccc2
Élie Meignan <contact@eliepse.fr>
2025-03-01 13:52:38 +0100

    Fix unsupported top level await

--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,46 @@
 import { App } from "./App";
 import { wait } from "./helpers";
 
-const appDom = document.querySelector("#app");
+async function main() {
+	function updateLoader(percent: number, title: string): void {
+		if (!loaderDom.loadingBar || !loaderDom.loadingCounter || !loaderDom.loadingTitle) {
+			return;
+		}
 
-if (null === appDom) {
-	throw new Error("App DOM missing");
-}
-
-const loaderDom = {
-	root: document.querySelector<HTMLDivElement>("#loader"),
-	loadingBar: document.querySelector<HTMLDivElement>(".loader__progressBar div"),
-	loadingCounter: document.querySelector<HTMLDivElement>(".loader__progressCounter"),
-	loadingTitle: document.querySelector<HTMLDivElement>(".loader__title"),
-};
-
-function updateLoader(percent: number, title: string): void {
-	if (!loaderDom.loadingBar || !loaderDom.loadingCounter || !loaderDom.loadingTitle) {
-		return;
+		loaderDom.loadingBar.style.width = `${percent.toFixed(2)}%`;
+		loaderDom.loadingCounter.innerText = `${Math.round(percent)} %`;
+		loaderDom.loadingTitle.innerText = title;
 	}
 
-	loaderDom.loadingBar.style.width = `${percent.toFixed(2)}%`;
-	loaderDom.loadingCounter.innerText = `${Math.round(percent)} %`;
-	loaderDom.loadingTitle.innerText = title;
+	const loaderDom = {
+		root: document.querySelector<HTMLDivElement>("#loader"),
+		loadingBar: document.querySelector<HTMLDivElement>(".loader__progressBar div"),
+		loadingCounter: document.querySelector<HTMLDivElement>(".loader__progressCounter"),
+		loadingTitle: document.querySelector<HTMLDivElement>(".loader__title"),
+	};
+
+	const appDom = document.querySelector("#app");
+
+	if (null === appDom) {
+		throw new Error("App DOM missing");
+	}
+
+	const app = new App(appDom);
+
+	await app.load((step, total, title) => updateLoader((step / total) * 100, title));
+
+	await wait(500);
+	updateLoader(100, "Ready");
+
+	void app.launch();
+
+	await wait(350);
+
+	loaderDom.root && (loaderDom.root.style.opacity = "0");
+
+	await wait(1000);
+
+	loaderDom.root?.remove();
 }
 
-const app = new App(appDom);
-
-await app.load((step, total, title) => updateLoader((step / total) * 100, title));
-
-await wait(500);
-updateLoader(100, "Ready");
-
-void app.launch();
-
-await wait(350);
-
-loaderDom.root && (loaderDom.root.style.opacity = "0");
-
-await wait(1000);
-
-loaderDom.root?.remove();
-
-
+void main();

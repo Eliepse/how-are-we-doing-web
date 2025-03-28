c3eb7402fd8167ef2e8a1fc72a97705549b7fa3f
Eliepse <contact@eliepse.fr>
2025-03-28 11:44:20 +0100

    Lighten red for better lisibility

--- src/Diagram/colors.ts
+++ src/Diagram/colors.ts
@@ -3,5 +3,5 @@ import { Color } from "../Engine2D/ValueObject/Color";
 export const colors = {
 	defaultWhite: Color.White,
 	dimmedWhite: Color.White.alpha(0.46),
-	selected: Color.Red,
+	selected: new Color(251, 35, 35),
 } as const;

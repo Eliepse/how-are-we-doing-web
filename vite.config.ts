368d9157fdf43fb3177296f9ba11e7bc110d028f
Élie Meignan <contact@eliepse.fr>
2025-04-07 21:48:30 +0200

    Fix missing wall parts

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,6 @@ export default defineConfig({
 		rollupOptions: {
 			input: {
 				main: resolve(__dirname, "src/index.html"),
-				wall: resolve(__dirname, "src/wall/index.html"),
 			}
 		}
 	},

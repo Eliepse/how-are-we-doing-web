086222c7fb88d1367a4b530265a18cdd459f59b4
Élie Meignan <contact@eliepse.fr>
2026-03-31 22:21:00 +0200

    feat: Add inter-determinants links

--- src/Diagram/Renderer/PathologyLinksRenderer.ts
+++ src/Diagram/Renderer/PathologyLinksRenderer.ts
@@ -11,19 +11,65 @@ export class PathologyLinkRenderer extends SVGNodeRenderer {
 		const node = vnode.node as Diagram;
 		const determinants = node.getDeterminants();
 		const shapes = this.getShapes(vnode);
+		const selectedNode = node.getSelectedNode();
+
+		determinants.forEach((determinant) => {
+			determinant.associations.determinants.forEach((assoDetId) => {
+				const shapeKey = `link:determinant:${determinant.id}-${assoDetId}`;
+				const assoDet = determinants.get(assoDetId);
+
+				if (undefined === assoDet) {
+					return;
+				}
+
+				if ("determinants" !== node.links) {
+					shapes.remove(shapeKey);
+					return;
+				}
+
+				if (selectedNode !== determinant && selectedNode !== assoDet) {
+					shapes.remove(shapeKey);
+					return;
+				}
+
+				const path = shapes.get(shapeKey, () => new LinkPath());
+
+				const center = this.renderer.size.div(2);
+
+				// Create a temporary node to compute the position
+				const tempNode = new Node2D();
+				tempNode.setPosition(determinantAnchorOffset);
+
+				tempNode.setParent(determinant);
+				const to = tempNode.getGlobalPosition().get();
+
+				tempNode.setParent(assoDet);
+				const from = tempNode.getGlobalPosition().get();
+
+				const fromAnchor = center.sub(from).rot(Angle.fromDeg(32)).mul(0.32).add(from);
+				const toAnchor = center.sub(to).rot(Angle.fromDeg(32)).mul(0.32).add(to);
+
+				path.updateMesh(from, fromAnchor, to, toAnchor);
+			});
+		});
 
 		node.getPathologies().forEach((pathology) => {
 			const pathologyActive = "selected" === pathology.status.get() || "preview" === pathology.status.get();
 
 			pathology.associations.determinants.forEach((detId) => {
 				const determinant = determinants.get(detId);
+				const shapeKey = `link:pathology:${pathology.id}-${detId}`;
 
 				if (undefined === determinant) {
 					return;
 				}
 
+				if ("pathologies" !== node.links) {
+					shapes.remove(shapeKey);
+					return;
+				}
+
 				const determinantActive = "selected" === determinant.status.get() || "preview" === determinant.status.get();
-				const shapeKey = `link:pathology:${pathology.id}-${determinant.id}`;
 
 				if (false === pathologyActive || false === determinantActive || "pathologies" !== node.links) {
 					shapes.remove(shapeKey);
@@ -46,7 +92,7 @@ export class PathologyLinkRenderer extends SVGNodeRenderer {
 
 				path.updateMesh(from.get(), fromAnchor, to.get(), toAnchor);
 
-				if(determinant.status.hasChanged()) {
+				if (determinant.status.hasChanged()) {
 					path.updateStyle("selected" === determinant.status.get() ? style.selected : style.preview);
 				}
 			});

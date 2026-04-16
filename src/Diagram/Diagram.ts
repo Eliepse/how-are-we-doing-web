937643dd5240690e5d31da93f8cf9414dc1832df
Élie Meignan <contact@eliepse.fr>
2026-04-16 16:32:31 +0200

    fix: Label doesn't hide when changing mode

--- src/Diagram/Diagram.ts
+++ src/Diagram/Diagram.ts
@@ -542,10 +542,9 @@ export class Diagram extends Node2D {
 			transitionNodeOpacity(families.facility, 500, new Opacity(.1));
 			transitionNodeOpacity(this.decorations, 500, new Opacity(.1));
 
-			this._selectedNode = this._selectedNode instanceof Determinant ? this._selectedNode : undefined;
-			this._previewedNode = this._previewedNode instanceof Determinant ? this._previewedNode : undefined;
+			this.selectNode(this._selectedNode instanceof Determinant ? this._selectedNode : undefined);
+			this.previewNode(this._previewedNode instanceof Determinant ? this._previewedNode : undefined);
 
-			this.updateNodesHighlight();
 			return;
 		}
 

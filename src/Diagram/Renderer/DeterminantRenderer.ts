24d37cc3805975d0cf6f9de4a37d35dee3477a32
Élie Meignan <contact@eliepse.fr>
2026-05-04 17:44:54 +0200

    feat: Display core on n+1 status

--- src/Diagram/Renderer/DeterminantRenderer.ts
+++ src/Diagram/Renderer/DeterminantRenderer.ts
@@ -71,7 +71,7 @@ export class DeterminantRenderer extends SVGNodeRenderer {
 			circle.updateStyle(new SVGStyle({ stroke: new Stroke({ width: 2, color: color.alpha(anchorOpacity) }) }));
 			circleCore.updateStyle(new SVGStyle({ fill: color }));
 
-			if ("selected" === status.get() || "preview" === status.get()) {
+			if ("selected" === status.get() || "preview" === status.get() || "n+1" === status.get()) {
 				circleCore.show();
 			} else {
 				circleCore.hide();
